82209404d4be5b1d1ba302afd1b7b3799513ac97
Amgos <79389391+Amgos362@users.noreply.github.com>
2023-08-05 16:16:37 +0900

    Update Upbot_new_ver8.py

--- Upbot_new_ver8.py
+++ Upbot_new_ver8.py
@@ -21,18 +21,23 @@ def calculate_indicator(df):
     df['indicator'] = df['indicator'].rolling(window=10).mean()
     return df['indicator'].iloc[-1]  # return the latest indicator
 
-def buy(coin, portion):
+avg_buy_price = {}
+
+def buy(coin, portion, vvs):
     krw = upbit.get_balance("KRW")
     if krw > 5000:
         upbit.buy_market_order(coin, krw * 0.9995 * portion)
         coin_amount = upbit.get_balance(coin[4:])
-        buy_message = f"Bought {coin}: {coin_amount} amount"
+        avg_buy_price[coin] = pyupbit.get_current_price(coin)
+        buy_message = f"매수 {coin}: {coin_amount}개, 평단 {avg_buy_price[coin]}원, VVS = {vvs.round(1)}"
         send_message(buy_message, MESSAGE_TOKEN) # 이 부분 추가
     return
 
-def sell(coin):
+def sell(coin, vvs):
+    if coin not in avg_buy_price:
+        return
     coin_amount_before = upbit.get_balance(coin[4:])
-    coin_value_before = coin_amount_before * pyupbit.get_current_price(coin)
+    coin_value_before = coin_amount_before * avg_buy_price.get(coin, 0)
     upbit.sell_market_order(coin, coin_amount_before)
     krw_after = upbit.get_balance("KRW")
     profit_loss = krw_after - coin_value_before
@@ -40,8 +45,10 @@ def sell(coin):
         profit_loss_percent = 0  # 또는 적절한 다른 값을 할당
     else:
         profit_loss_percent = (profit_loss / coin_value_before) * 100
-    sell_message = f"Sold {coin}: {coin_amount_before} amount, Profit/Loss: {profit_loss} KRW ({profit_loss_percent}%)"
+    sell_message = f"매도 {coin}: {coin_amount_before}개, 손익금 {profit_loss}원 ({profit_loss_percent}%), VVS = {vvs.round(1)}"
     send_message(sell_message, MESSAGE_TOKEN) # 이 부분 추가
+    if coin in avg_buy_price:
+        del avg_buy_price[coin]
     return
 
 def send_message(message, token=None):
@@ -73,12 +80,13 @@ while True:
         df = pyupbit.get_ohlcv(ticker=coin, interval=intervals[i])
         now_indicator = calculate_indicator(df)
 
-        if now_indicator <= 10 and not buy_flag[i]:  # indicator가 10 이하일 때 매수
+
+        if now_indicator <= 10 and not buy_flag[i] and coin not in avg_buy_price:  # indicator가 10 이하일 때 매수
             portion = 1/3 if coin == "KRW-BTC" else 2/3
-            buy(coin, portion)
+            buy(coin, portion, now_indicator)
             buy_flag[i] = True  # 매수 플래그 설정
-        elif now_indicator >= 90 and not sell_flag[i]:  # indicator가 90 이상일 때 매도
-            sell(coin)
+        elif now_indicator >= 90 and not sell_flag[i] and coin in avg_buy_price:  # indicator가 90 이상일 때 매도
+            sell(coin, now_indicator)
             sell_flag[i] = True  # 매도 플래그 설정
 
         # 조건을 리셋하고 싶은 경우 (예: 매수 후 다시 매도하고 싶다면)
