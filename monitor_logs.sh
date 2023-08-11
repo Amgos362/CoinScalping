6d553b3f68a2e26e3bce6baf29bb6effaaf629ac
Amgos <79389391+Amgos362@users.noreply.github.com>
2023-08-11 16:45:56 +0900

    Update monitor_logs.sh

--- monitor_logs.sh
+++ monitor_logs.sh
@@ -3,5 +3,5 @@
 while true; do
     # 로그 파일에 변경이 감지되면 Python 스크립트 실행
     inotifywait -e modify /path/to/output.log
-    python3 /path/to/send_to_line.py
+    python3 /path/to/aws_cloudwatch.py
 done
